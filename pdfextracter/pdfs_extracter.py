b51c25aecc4d85430ae0c6cf6705abedc699674d
YohannesDatasci <yohannesdatasci2@gmail.com>
2020-03-24 17:08:18 +0400

    bug fix

--- pdfextracter/pdfs_extracter.py
+++ pdfextracter/pdfs_extracter.py
@@ -11,22 +11,25 @@ class PDFextract:
         return text
 
     # cleaning the string
-    def remove_numbers_and_lines(self, text):
-        # remove all non-armenian letters
-        text = re.sub(u"[^\u0530-\u058f0-9\s]", "", text)
-        text = "".join([x for x in text if not x.isdigit()])
+    def pdf_preprocessing(self, text):
+        # remove all non-armenian letters more than one spaces and split by :
+        text = re.sub(u"[^\u0530-\u058f:\s]", "", text)
         text = text.replace("\n", "")
         text = re.sub("  +", " ", text)
+        text = text.replace(":", "։")
         text = text.split("։")
         return text
 
     # writing data to the txt file
     def write_in_file(self, my_list, current_txt_heading):
-        # check if text is more than 100 bytes
-        if len(max(my_list, key=len)) > 100:
-            with open(current_txt_heading, "w", encoding="utf-8") as f1:
-                for list_item in my_list:
-                    f1.write(list_item + "\n")
+
+        # check if text is more than 100 lines
+        if len(my_list) > 100:
+            # check if the biggest line is at least 30 characters
+            if len(max(my_list, key=len)) > 30:
+                with open(current_txt_heading, "w", encoding="utf-8") as f1:
+                    for list_item in my_list:
+                        f1.write(list_item + "\n")
 
 
 # list all names from pdfs/ folder
@@ -36,10 +39,11 @@ pdf = PDFextract()
 
 
 for current_name in list_of_pdfs:
+
     current_pdf = "pdfs/" + current_name[0:-4] + ".pdf"
     current_txt = "cors/" + current_name[0:-4] + ".cor"
     current_text = pdf.extract_pdf_text(current_pdf)
 
     # check if the text is not empty
     if current_text:
-        pdf.write_in_file(pdf.remove_numbers_and_lines(current_text), current_txt)
+        pdf.write_in_file(pdf.pdf_preprocessing(current_text), current_txt)
